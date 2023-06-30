39668de037489abd32581699135e069c6444a0bd
ahmakbar <ahmakbarr48@gmail.com>
2023-06-30 17:57:29 +0700

    Update Frontend yang belum selesai

--- resources/views/kasir/dt/aksi.blade.php
+++ resources/views/kasir/dt/aksi.blade.php
@@ -1,8 +1,8 @@
-<form class="flex column justify-center" action="{{ route('orders.destroy', $model->order_id) }}" method="POST">
+<form class="form-aksi flex column justify-center" action="{{ route('orders.destroy', $model->order_id) }}" method="POST">
     @csrf
     @method('DELETE')
-    <button class="button-aksi hapus" style="font-size: 12px;" type="submit">Hapus</button>
     {{-- <button class="btn btn-danger" type="button">Edit</button> --}}
-    <a class="button-aksi selesai" style="font-size: 12px;" href="{{ route('orders.done', $model->order_id) }}">Selesai</a>
-    <a class="button-aksi diproses" style="font-size: 12px;" href="{{ route('orders.proses', $model->order_id) }}">Diproses</a>
+    <a class="button-aksi diproses" style="" href="{{ route('orders.proses', $model->order_id) }}">Diproses</a>
+    <a class="button-aksi selesai" style="" href="{{ route('orders.done', $model->order_id) }}">Selesai</a>
+    <button class="button-aksi hapus" style="" type="submit">Hapus</button>
 </form>
